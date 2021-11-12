2a5a0a0fa0d5b6d89c26778bc2863871ba5560c8
unknown <ghaliarose89@gmail.com>
2021-11-12 16:13:17 -0600

    addRole update

--- server.js
+++ server.js
@@ -48,7 +48,9 @@ function startApp() {
     }
   });}
 
-
+  async function showDepartment(){
+   
+  }
 function viewAllDep() {
   const sql = `SELECT * FROM department`;
   db.query(sql, (err, row) => {
@@ -102,51 +104,50 @@ function addDep() {
 
 
 function addRole() {
-  
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "Please enter the Role name",
-      name: "roleName",
-    },
-
-    {
-      type: "input",
-      message: "Enter  the Salary for this role",
-      name: "salaryNum",
-    },
-    {
-      type: "list",
-      name:'depChoice',
-      message: "Please Choose the Department for this role",
-      choices: showDepartment()
-      
-    },
-  ])
-    .then(data => {
-      const roleName = data.roleName;
-      const depName = showDepartment().indexOf(data.depChoice) + 1;
-      const salaryNum = data.salaryNum;
-     
-      const sql = `INSERT INTO role (title,salary ,department_id) valueS (?,?,?) `;
-      const value = [roleName, salaryNum, depName];
-      db.query(sql, value, (err, rows) => {
-        if (err) throw err;
-        console.log('New department has been created');
-      })
-    });
-};
-
-function showDepartment(){
   let dep =[];
   db.query(('SELECT * FROM department'), (err,rows)=>{
     if (err) console.log (err);
       dep = rows.map(rows => dep.push(rows.dep_name));
-    // console.log (dep);
-    // console.log(rows);
-    return dep;
+      console.log(dep);
+      inquirer.prompt([
+        {
+          type: "input",
+          message: "Please enter the Role name",
+          name: "roleName",
+        },
+    
+        {
+          type: "input",
+          message: "Enter  the Salary for this role",
+          name: "salaryNum",
+        },
+        {
+          type: "list",
+          name:'depChoice',
+          message: "Please Choose the Department for this role",
+          choices: dep,
+          
+        },
+      ])
+        .then(data => {
+          const roleName = data.roleName;
+          const depName = data.depChoice + 1;
+          const salaryNum = data.salaryNum;
+         
+          const sql = `INSERT INTO role (title,salary ,department_id) valueS (?,?,?) `;
+          const value = [roleName, salaryNum, depName];
+          db.query(sql, value, (err, rows) => {
+            if (err) throw err;
+            console.log('New Role has been created');
+          })
+        });
+    
   })
-}
+  
+  
+};
+
+
 
 
 
