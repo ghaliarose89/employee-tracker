1f9b327250e231e9f8b3209fc5127355bb6ff34d
unknown <ghaliarose89@gmail.com>
2021-11-14 11:30:40 -0600

    final touchs up

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const db = require('./db/connection');
+const db = require('./config/connection');
 const figlet = require('figlet');
 const chalk = require('chalk');
 
